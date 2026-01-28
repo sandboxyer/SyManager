d0fb0f107f67e9a4059c99f96ea2a9387d7ab871
Daniel Massud <massudstudio@gmail.com>
2026-01-28 12:47:48 -0500

    SyManager.js, db,pm e app embending na classe principal, e handle para abrir o menu apenas com a execução direta

--- SyManager.js
+++ SyManager.js
@@ -1,12 +1,25 @@
 import TerminalHUD from '././._/._/._/Util/TerminalHUD.js'
 import MainMenu from './._/._/LegacyHUD/MainMenu.js'
+import SyDB from './._/SyDB.js'
+import SyPM from './._/SyPM.js'
+import SyAPP from './._/SyAPP.js'
+
 
 class SyManager {
 
+static DB = SyDB
+static PM = SyPM
+static APP =  SyAPP
+
 }
 
-let HUD = new TerminalHUD()
+export default SyManager
 
-HUD.displayMenu(MainMenu)
 
-export default HUD
+if (import.meta.url.endsWith(process.argv[1]) || process.argv[1] === import.meta.url.replace('file://', '')) {
+    let args = process.argv.slice(2)
+    if (args.length === 0) {
+        let HUD = new TerminalHUD()
+        HUD.displayMenu(MainMenu)
+    }
+}
